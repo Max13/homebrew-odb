acdeccd283cbe0f5d3c2ec7dbe771ac440cc8b78
Adnan RIHAN <adnan@rihan.fr>
2015-03-18 12:43:49 +0100

    libodb-sqlite 2.4.0 (new formula)

---
+++ libodb-sqlite.rb
@@ -0,0 +1,63 @@
+class LibodbSqlite < Formula
+  homepage "http://www.codesynthesis.com/products/odb/"
+  url "http://www.codesynthesis.com/download/odb/2.4/libodb-sqlite-2.4.0.tar.gz"
+  sha256 "cd687c882a8dc14ded4eb160e82de57e476b1feef5c559c5a6a5c7e671a10cf4"
+
+  option "with-libstdc++",
+         "Force compiling with libstdc++ (Default BEFORE 10.9)"
+  option "with-libc++",
+         "Force compiling with libc++ (Default SINCE 10.9)"
+  option "with-gcc",
+         "Force compiling with gcc and GCC's libstdc++"
+
+  if build.with?("gcc")
+    depends_on "gcc"
+
+    fails_with :clang
+    fails_with :llvm
+
+    STDLIB = "gcc"
+  elsif build.with?("libstdc++")
+    STDLIB = "libstdc++"
+  elsif build.with?("libc++")
+    STDLIB = "libc++"
+  else
+    STDLIB = MacOS.version < :mavericks ? "libstdc++" : "libc++"
+  end
+
+  depends_on "libodb" => "with-" + STDLIB
+
+  def install
+    args = %W[
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+    ]
+
+    if STDLIB != "gcc"
+      args << "CXX=#{ENV.cxx}\ -stdlib=#{STDLIB}"
+    end
+
+    system "./configure", *args
+    system "make", "install"
+
+    opoo <<-EOS.undent
+      Your app, libodb and its libs must be compiled with the same
+      C++ standard library. Currently: #{STDLIB}
+    EOS
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS.undent
+      #include <odb/sqlite/exceptions.hxx>
+      int main()
+      {
+        try {
+          throw odb::sqlite::forced_rollback();
+        } catch (const odb::sqlite::forced_rollback &e) {}
+        return 0;
+      }
+    EOS
+    system ENV.cxx, "test.cpp", "-o", "test", "-lodb-sqlite", "-lodb"
+    system "./test"
+  end
+end
