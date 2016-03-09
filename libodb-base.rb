20915223b0d1389b71a3ccb31a61e14d233bc4e1
Jacob Meacham <jacob.e.meacham@gmail.com>
2016-03-08 18:30:56 -0800

    Add base formula for lib-odb connectors. Add connectors for postgres, mssql, mysql, and oracle.

---
+++ libodb-base.rb
@@ -0,0 +1,61 @@
+class LibodbBase < Formula
+  url "http://www.codesynthesis.com/download/odb/2.4/libodb-2.4.0.tar.gz.sha1"
+  sha256 "2d26995974eb03b6ba6d26c32f9956e95d5a3d0db6b5fa149a2df7e4773112f4"
+  homepage "http://www.codesynthesis.com/products/odb/"
+
+  option "with-libstdc++",
+         "Force compiling with libstdc++ (Default BEFORE 10.9)"
+  option "with-libc++",
+         "Force compiling with libc++ (Default SINCE 10.9)"
+  option "with-gcc",
+         "Force compiling with gcc and GCC's libstdc++"
+
+  if build.with?("gcc")
+    depends_on "gcc"
+
+    fails_with :clang
+    fails_with :llvm
+
+    STDLIB = "gcc"
+  elsif build.with?("libstdc++")
+    STDLIB = "libstdc++"
+  elsif build.with?("libc++")
+    STDLIB = "libc++"
+  else
+    STDLIB = MacOS.version < :mavericks ? "libstdc++" : "libc++"
+  end
+
+  depends_on "libodb" => "with-" + STDLIB
+
+  def standard_install
+    args = %W[
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+    ]
+
+    if STDLIB != "gcc"
+      args << "CXX=#{ENV.cxx}\ -stdlib=#{STDLIB}"
+    end
+
+    system "./configure", *args
+    system "make", "install"
+
+    opoo <<-EOS.undent
+      Your app, libodb and its libs must be compiled with the same
+      C++ standard library. Currently: #{STDLIB}
+    EOS
+  end
+
+  def install
+    onoe <<-EOS.undent
+      Do not install this formula. Instead, install the formula for the database
+      you wish to use.
+    EOS
+    exit 1
+  end
+
+  test do
+    raise
+  end
+
+end
---
+++ libodb-mssql.rb
@@ -0,0 +1,24 @@
+require_relative "libodb-base"
+class LibodbMssql < LibodbBase
+  url "http://www.codesynthesis.com/download/odb/2.4/libodb-mssql-2.4.0.tar.gz"
+  sha256 "7863ce814c144cbd464709018007918c19ba71760e484022faa2fa5bf40bdfe7"
+
+  def install
+    standard_install
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS.undent
+      #include <odb/mssql/exceptions.hxx>
+      int main()
+      {
+        try {
+          throw odb::mssql::database_exception();
+        } catch (const odb::mssql::database_exception &e) {}
+        return 0;
+      }
+    EOS
+    system ENV.cxx, "test.cpp", "-o", "test", "-lodb-mssql", "-lodb"
+    system "./test"
+  end
+end
